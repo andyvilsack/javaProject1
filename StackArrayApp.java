8578ac67978d9f00d838c680b2bd1aabe87dafbd
andyvilsack <andy.vilsack@gmail.com>
2017-02-22 20:23:52 -0500

    got push to work and some test stuff in the app to see if everythings good.  Looks good for now, but may need more testing in the future

--- StackArrayApp.java
+++ StackArrayApp.java
@@ -3,10 +3,25 @@ public class StackArrayApp {
 
 	public static void main(String[] args)
 	{
-		Person guy = new Person();
+		Person guy = new Person("Jew", "Lastname1", 1, 1.00);
+		Person guy2 = new Person("Dog", "Lastname2", 2, 2.00);
+		Person guy3 = new Person("Andy", "Lastname3", 3, 3.00);
+		Person guy4 = new Person("Eat", "Lastname4", 4, 4.00);
+		Person guy5 = new Person("Butt", "Lastname5", 5, 5.00);
+		Person guy6 = new Person("John", "Lastname6", 6, 6.00);
+		Person guy7 = new Person("-", "Lastname7", 7, 7.00);
+		Person guy8 = new Person("George", "Lastname8", 8, 6.00);
 		StackArray myList = new StackArray();
 		myList.push(guy);
-		myList.push(guy);
+		myList.push(guy2);
+		myList.push(guy3);
+		myList.push(guy4);
+		myList.push(guy5);
+		myList.push(guy6);
+		myList.push(guy7);
+		myList.push(guy8);
 		System.out.println(myList);
+		
+
 	}
 }
--- stackArray.java
+++ stackArray.java
@@ -11,6 +11,13 @@ public class StackArray {
 		stackArray[0]=new Person();
 		stackArray[1]=new Person();
 	}
+	StackArray(int num)
+	{
+		for(int i = 0; i<num-1; i++)
+		{
+			stackArray[i]=new Person();
+		}
+	}
 	
 	public boolean isEmpty()
 	{
@@ -24,21 +31,37 @@ public class StackArray {
 	{
 		if(top==max-1)
 		{
+			int topHold = top;
 			int count = 0;
 			max = max * 2;
-			Person tempArray[] = new Person[max];
-			Person tempArray2[] = new Person[max];
-			for(int i=0;i<max;i++)
+			Person tempArray[] = new Person[max/2];
+			Person tempArray2[] = new Person[max/2];
+			for(int i=0;i<(max/2);i++)
 			{
 				tempArray[i] = this.pop();
 			}
-			for(int j=max;j>0;j--)
+			for(int j=(max/2)-1;j>=0;j--)
 			{
 				tempArray2[count] = tempArray[j];
 				count++;
 			}
 			
-			
+			Person stackArraycopy[] = new Person[max];
+			for(int k = 0; k<=topHold;k++)
+			{
+				stackArraycopy[k] = tempArray2[k];
+			}
+			for (int l = topHold+1; l<max;l++)
+			{
+				stackArraycopy[l]=new Person();
+			}
+			this.stackArray = new Person[max];
+			for (int m = 0; m<max;m++)
+			{
+				this.stackArray[m] = stackArraycopy[m];
+			}
+			this.top = topHold;
+			this.push(obj);
 		}
 		else
 		{
