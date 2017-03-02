4085535b0333a42037ee5491c8b9d02d92614122
John-George <johngeorgesample@gmail.com>
2017-03-01 23:07:51 -0500

    tempStack, myListCopy

--- StackArrayApp.java
+++ StackArrayApp.java
@@ -24,6 +24,9 @@ public class StackArrayApp {
 		System.out.println(myList);
 
 		System.out.println("data2.txt popped");
+//		for(int i = 0; i <= 5; i++) {
+//			System.out.println(myList.pop());
+//		}
 		System.out.println(myList.pop());
 		System.out.println(myList.pop());
 		System.out.println(myList.pop());
@@ -32,28 +35,60 @@ public class StackArrayApp {
 		
 		System.out.println("\n");
 		
-		StackArray newStack = new StackArray();
+		StackArray tempStack = new StackArray();
 		System.out.print("myList is empty:");
 		System.out.println(myList.isEmpty());
-		System.out.print("newStack is empty:");
-		System.out.println(newStack.isEmpty());
+		System.out.print("tempStack is empty:");
+		System.out.println(tempStack.isEmpty());
 		
 		boolean hasMoreData = true;
-		while (hasMoreData == true) {
+		while (hasMoreData == true) { //pop remaining values off myList and push them on tempStack
 			if(myList.isEmpty() == false) {
-				newStack.push(myList.pop());
+				tempStack.push(myList.pop());
 			}
 			else {
 				hasMoreData = false;
 			}
 		}
+	
+	
+		StackArray myListCopy = new StackArray();
+		
+		boolean x = true;
+		while (x == true) { //pop values off tempStack and push them on myList
+			if(tempStack.isEmpty() == false) {
+				myListCopy.push(tempStack.pop());
+			}
+			else {
+				x = false;
+			}
+		}
+		
+		
+//		System.out.println("\nmyListCopy:");
+//		while(myListCopy.isEmpty() == false) { //#8
+//			System.out.println(myListCopy.pop());
+//			
+//		}
+		
+		
 		
 		System.out.println("\nmylist:");
 		System.out.println(myList);
 
-		System.out.println("\nnewStack:");
-		System.out.println(newStack);
-
+		System.out.println("\ntempStack:");
+		System.out.println(tempStack);
+		
+		System.out.println("\nmyListCopy:");
+		System.out.println(myListCopy);
+		
+		
+			System.out.print("myList is empty:");
+			System.out.println(myList.isEmpty());
+			System.out.print("tempStack is empty:");
+			System.out.println(tempStack.isEmpty());
+			System.out.print("myListCopy is empty:");
+			System.out.println(myListCopy.isEmpty());
 	}
 }
 
