046c3420009c0a988be65541bee694948a3c1a3f
John-George <johngeorgesample@gmail.com>
2017-03-02 10:04:25 -0500

    documentation

--- stackArray.java
+++ stackArray.java
@@ -20,15 +20,17 @@ public class StackArray {
 			stackArray[i]=new Person();
 		}
 	}
-	
+	//checks to see if stack is empty
 	public boolean isEmpty()
 	{
 		return (top==-1);
 	}
+	//checks to see if stack is full, added for redundancy
 	public boolean isFull()
 	{
 		return false; //will never be true, always growing
 	}
+	//pushes the object passed into in onto the stack
 	public void push(Person obj)
 	{
 		if(top==max-1)
@@ -38,31 +40,36 @@ public class StackArray {
 			max = max * 2;
 			Person tempArray[] = new Person[max/2];  //The array is half full of nothing, don't search that
 			Person tempArray2[] = new Person[max/2];  //The array is half full of nothing, don't search that
+			//pop everything off of the array onto a temp
 			for(int i=0;i<(max/2);i++)
 			{
 				tempArray[i] = this.pop();
 			}
+			//flip all of those onto another temp so its in the correct order
 			for(int j=(max/2)-1;j>=0;j--)
 			{
 				tempArray2[count] = tempArray[j];
 				count++;
 			}
-			
+			//make a new stackarray and load the elements from the temp array
 			Person stackArraycopy[] = new Person[max];
 			for(int k = 0; k<=topHold;k++)
 			{
 				stackArraycopy[k] = tempArray2[k];
 			}
+			//fill the rest of the stackarray with dummy items
 			for (int l = topHold+1; l<max;l++)
 			{
 				stackArraycopy[l]=new Person();
 			}
+			//assign the temp stackarray to the object's stack array
 			this.stackArray = new Person[max];
 			for (int m = 0; m<max;m++)
 			{
 				this.stackArray[m] = stackArraycopy[m];
 			}
 			this.top = topHold;
+			//recursively call push to put the object on the stack
 			this.push(obj);
 		}
 		else
